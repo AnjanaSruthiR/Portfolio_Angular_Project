aa5135cf6cdaba748dc12c08a41bd0e6bd2aecd3
Anjana Sruthi R <ranga.anj@northeastern.edu>
2024-10-31 02:53:34 -0400

    Commiting the app component HTML,CSS and typescript files

--- my-angular-app/src/app/app.component.ts
+++ my-angular-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +6,33 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-angular-app';
+
+  // Reference to the app wrapper and header elements
+  @ViewChild('appWrapper', { static: true }) appWrapper!: ElementRef;
+  @ViewChild('appHeader', { static: true }) appHeader!: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    // Set light theme by default
+    this.renderer.addClass(this.appWrapper.nativeElement, 'light-theme');
+  }
+
+  toggleTheme() {
+    const isDarkMode = this.appWrapper.nativeElement.classList.contains('dark-theme');
+
+    if (isDarkMode) {
+      this.renderer.removeClass(this.appWrapper.nativeElement, 'dark-theme');
+      this.renderer.addClass(this.appWrapper.nativeElement, 'light-theme');
+      console.log("Switched to Light Theme");
+    } else {
+      this.renderer.removeClass(this.appWrapper.nativeElement, 'light-theme');
+      this.renderer.addClass(this.appWrapper.nativeElement, 'dark-theme');
+      console.log("Switched to Dark Theme");
+    }
+  }
 }
